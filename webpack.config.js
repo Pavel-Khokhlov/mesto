07f81c9e285f89369bbd11df63b8b0f941174f7b
Pavel Khokhlov <ya.pavel-kh@yandex.ru>
2020-12-05 02:07:54 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "./",
+    publicPath: "http://pavel-khokhlov.github.io/mesto",
   },
 
   module: {
